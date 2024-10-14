295a96579527b1f213faa07d24f8a52cefc7c152
Julxyk02 <marceloquinones832@gmail.com>
2024-10-14 17:23:34 -0500

    Primer commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleApp2;
+
+gato g = new gato();
+g.Nombre = "Michi";
+g.Edad = 5;
+Console.WriteLine($"Mi gato {g.Nombre}, tiene {g.Edad} años");
---
+++ ConsoleApp2/gato.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2 {
+    internal class gato {
+        private string nombre;
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        private int edad;
+        public int Edad { get => edad; set => edad = value; }
+
+
+    }
+}
